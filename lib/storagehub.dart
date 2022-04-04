f1f0dfdcceac72ac81db5b914222f71d2ccc5666
foysalmia <mfoysal314@gmail.com>
2022-04-04 17:17:03 +0600

    question added in the UI

---
+++ lib/storagehub.dart
@@ -0,0 +1,112 @@
+var questions = [
+  {
+    'question': "What's your name ?",
+    'answer': ['Tim', 'David', 'Alex', 'Arnold']
+  },
+  {
+    'question': "Who is your favourite player in football?",
+    'answer': ['NJr10', 'CR7', 'LM10', 'AB1']
+  },
+  {
+    'question': "Who is the best player in the universe?",
+    'answer': ['Gayle', 'Kohli', 'Warner', 'Fince', 'Ami Nije', 'Jani na']
+  },
+  {
+    'question': "Whom do you love most ?",
+    'answer': ['GF', 'Sports', 'PC', 'Mobile']
+  },
+  {
+    'question': "Which you support in football?",
+    'answer': [
+      'Argentina',
+      'France',
+      'Brasil',
+      'Spain',
+      'England',
+      'Belgium',
+      'Germany'
+    ]
+  },
+  {
+    'question': "Which club do you like most ?",
+    'answer': [
+      'Real Madrid',
+      'PSG',
+      'Liver Pool',
+      'MANU',
+      'MAN City',
+      'Barcelona'
+    ]
+  },
+  {
+    'question': "Who is the best coach in club football ?",
+    'answer': [
+      'Takla Gardiwala',
+      'Takla Gidan',
+      'Master Mind klop',
+      'Ancelti',
+      'Xavi',
+      'Tucheal'
+    ]
+  },
+  {
+    'question': "Who is best GK right now?",
+    'answer': [
+      'Alisson',
+      'Ederson',
+      'M Neuer',
+      'Terstagen',
+      'Oblack',
+      'Dunarumma',
+      'Navas'
+    ]
+  },
+  {
+    'question': "Who is the best captain in club football right now?",
+    'answer': [
+      'Benzama',
+      'Markunius',
+      'Robertson',
+      'Farnandinho',
+      'Jamal Bhuiya'
+    ]
+  },
+  {
+    'question': "Which team will win UCL this season?",
+    'answer': [
+      'Real Madrid ❤',
+      'Bayern',
+      'Mancity',
+      'ManU',
+      'Chelsea',
+      'Liverpool',
+      'Barceloan😂'
+    ]
+  },
+  {
+    'question': "Which team will win Qatar World cup?",
+    'answer': [
+      'Brasil',
+      'Uruguay',
+      'Germany',
+      'France',
+      'Netherland',
+      'Argentina 😂'
+    ]
+  },
+  {
+    'question': "Do you belive Messi is from another world?",
+    'answer': ['😂', 'No', 'Yes', 'Ha Ha Ha Ha Ha Ha ']
+  },
+  {
+    'question': "Which sport do you like most?",
+    'answer': [
+      'Football',
+      'Cricket',
+      'Vollyball',
+      'Pubg',
+      'free fire',
+      'I don\'t like sports'
+    ]
+  }
+];
